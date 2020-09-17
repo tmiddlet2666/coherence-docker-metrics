9db479ad510b4b374fefa380c960d99f22e2465b
Tim Middleton <tim.middleton@oracle.com>
2020-09-17 15:34:45 +0800

    Add script to download dashboards for Linux/Mac

---
+++ grafana/download-dashboard.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+for file in alerts-dashboard.json          coherence-dashboard-main.json        federation-details-dashboard.json  http-servers-summary-dashboard.json  member-details-dashboard.json       proxy-server-detail-dashboard.json    services-summary-dashboard.json cache-details-dashboard.json   dashboards.yaml                      federation-summary-dashboard.json  kubernetes-summary-dashboard.json    members-summary-dashboard.json      proxy-servers-summary-dashboard.json caches-summary-dashboard.json  elastic-data-summary-dashboard.json  grpc-summary-dashboard.json        machines-summary-dashboard.json      persistence-summary-dashboard.json  service-details-dashboard.json 
+do
+   curl https://raw.githubusercontent.com/oracle/coherence-operator/master/dashboards/grafana/${file} -o dashboards/${file}
+done
