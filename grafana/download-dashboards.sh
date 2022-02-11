f5a2d746d716cc7dd50585a84fec55dd4a4d3438
Tim Middleton <tim.middleton@oracle.com>
2022-02-11 08:33:00 +0800

    add executor dashboards

--- grafana/download-dashboards.sh
+++ grafana/download-dashboards.sh
@@ -4,8 +4,7 @@ for file in alerts-dashboard.json coherence-dashboard-main.json federation-detai
             member-details-dashboard.json proxy-server-detail-dashboard.json services-summary-dashboard.json cache-details-dashboard.json \
             federation-summary-dashboard.json  kubernetes-summary-dashboard.json members-summary-dashboard.json proxy-servers-summary-dashboard.json \
             caches-summary-dashboard.json elastic-data-summary-dashboard.json grpc-summary-dashboard.json machines-summary-dashboard.json \
-            persistence-summary-dashboard.json service-details-dashboard.json 
+            persistence-summary-dashboard.json service-details-dashboard.json coherence-executors-summary.json coherence-executor-detail.json
 do
    curl https://raw.githubusercontent.com/oracle/coherence-operator/master/dashboards/grafana/${file} -o dashboards/${file}
-
 done
